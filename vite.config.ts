c574f3cd752820a8dd2fe1f48ede4f79c8468ea5
leo.lu <lqz.leo@gmail.com>
2022-12-07 21:35:33 +0800

    updat eversion

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path, { resolve } from "path";
-
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()
+  plugins: [vue(),
 
-
+    vueJsx()
 
   ],
   resolve: {
